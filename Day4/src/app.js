fda45b50d7edcd02e6966530eb215a896cfc9e67
Devasshish <devashishmac13@gmail.com>
2026-02-08 23:42:05 +0530

    Day 4 code pushed

---
+++ Day4/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+const notes = []    
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+    res.send("Note created")
+})
+
+app.get('/notes',(req,res)=>{
+    res.send(notes)
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index] 
+    console.log(req.params)
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    notes[req.params.index].desc=req.body.desc
+})
+module.exports = app
