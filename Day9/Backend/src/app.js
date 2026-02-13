e1b8b7ed63f4498d53a6fb56818778ee29e8af34
Devasshish <devashishmac13@gmail.com>
2026-02-13 15:45:04 +0530

    code pushed

--- Day9/Backend/src/app.js
+++ Day9/Backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 app.use(express.json())
 app.use(cors())
+app.use(express.static("./public"))
 const noteModel = require('./model/notes.model')
+
 
 // POST
 app.post('/notes',async (req, res) => {
@@ -47,6 +50,42 @@ app.patch('/notes/:id',async (req,res)=>{
     res.status(200).json({
         message:"modified data",
     })
+})
+
+app.put('/notes/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const { title, desc } = req.body
+
+    const updatedNote = await noteModel.findByIdAndUpdate(
+      id,
+      { title, desc },
+      { new: true }   // updated data return karega
+    )
+
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" })
+    }
+
+    res.status(200).json({
+      message: "Note updated successfully",
+      note: updatedNote
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message
+    })
+  }
+})
+
+
+// wild card 
+
+app.use("*name",(req,res)=>{
+  // res.send("Mejsbhuj")
+  res.sendFile(path.join(__dirname,"..",'public/index.html'))
 })
 
 module.exports = app
--- Day9/Frontend/src/App.jsx
+++ Day9/Frontend/src/App.jsx
@@ -3,26 +3,8 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [note, setNote] = useState([{
-    title: "title 1",
-    desc: "Desc 1"
-  },
-  {
-    title: "title 2",
-    desc: "Desc 2"
-  },
-  {
-    title: "title 3",
-    desc: "Desc 3"
-  },
-  {
-    title: "title 4",
-    desc: "Desc 4"
-  },
-  ])
-
-  useEffect(() => {
-
+  const [note, setNote] = useState([])
+  function getData() {
     axios.get('http://localhost:3000/notes')
       .then((res) => {
         setNote(res.data.note)
@@ -31,15 +13,77 @@ const App = () => {
         console.log("Not get");
 
       })
-  })
+  }
+  useEffect(() => {
+    getData()
+
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const { title, desc } = e.target.elements
+    console.log(title.value, desc.value);
+
+    axios.post('http://localhost:3000/notes', {
+      title: title.value,
+      desc: desc.value
+    })
+      .then((res) => {
+        console.log(res.data);
+
+        getData()
+      })
+  }
+
+  function handleDelete(noteId) {
+    console.log(noteId);
+    axios.delete(`http://localhost:3000/notes/${noteId}`)
+      .then(() => {
+
+        getData()
+      })
+  }
+
+  function handleUpdate(noteId) {
+    console.log(noteId);
+    
+    const newTitle = prompt("Enter new title")
+    const newDesc = prompt("Enter new description")
+    axios.put('http://localhost:3000/notes/'+noteId,
+      {
+        title : newTitle,
+        desc : newDesc
+      }
+    )
+    .then(()=>{
+      getData()
+    })
+  }
+
   return (
     <div className='w-full h-screen bg-black text-white'>
-
-      {
-        note.map((note) => {
-          return note.title
-        })
-      }
+      <form className='p-2 flex justify-center gap-2 focus:border-0 target:border-0 active:border-0' onSubmit={handleSubmit}>
+        <input name='title' className='bg-white p-2 text-black border-0'></input>
+        <input name='desc' className='bg-white p-2 text-black border-0'></input>
+        <button className='border px-3'>Submit</button>
+      </form>
+      <div className="flex flex-wrap justify-center mt-3">
+        {
+          note.map((note) => {
+            return <div className='p-3 rounded-2xl gap-4 border-2 m-2 w-50 h-fit'>
+              <h1>{note.title}</h1>
+              <h2>{note.desc}</h2>
+              <button className='bg-green-600 mr-5 mt-2 p-1 px-3 rounded-md' onClick={() => {
+                handleUpdate(note._id)
+              }}>Update</button>
+              <button className='bg-red-600 p-1 px-3 rounded-md' onClick={() => {
+                handleDelete(note._id)
+              }}>Delete</button>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
